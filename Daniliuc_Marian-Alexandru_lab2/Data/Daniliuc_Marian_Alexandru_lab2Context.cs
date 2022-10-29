9076808a8e44e1630387f1ff92710e7bcd4f3bbc
alescandruu <alex.daniliuc14@yahoo.com>
2022-10-29 21:47:23 +0300

    Add project files.

---
+++ Daniliuc_Marian-Alexandru_lab2/Data/Daniliuc_Marian_Alexandru_lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Daniliuc_Marian_Alexandru_lab2.Models;
+
+namespace Daniliuc_Marian_Alexandru_lab2.Data
+{
+    public class Daniliuc_Marian_Alexandru_lab2Context : DbContext
+    {
+        public Daniliuc_Marian_Alexandru_lab2Context (DbContextOptions<Daniliuc_Marian_Alexandru_lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Daniliuc_Marian_Alexandru_lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Daniliuc_Marian-Alexandru_lab2/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Daniliuc_Marian_Alexandru_lab2.Data;
+using Daniliuc_Marian_Alexandru_lab2.Models;
+
+namespace Daniliuc_Marian_Alexandru_lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Daniliuc_Marian_Alexandru_lab2.Data.Daniliuc_Marian_Alexandru_lab2Context _context;
+
+        public IndexModel(Daniliuc_Marian_Alexandru_lab2.Data.Daniliuc_Marian_Alexandru_lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book.ToListAsync();
+            }
+        }
+    }
+}
